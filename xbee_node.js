48a60bcc912f48d8fad940312900934a5b71dea8
MY24 <MY24Email@gmail.com>
2012-02-13 00:54:19 -0500

    version 5

--- xbee_node.js
+++ xbee_node.js
@@ -9,7 +9,7 @@ var app = express.createServer()
 //http://www.laptop.lan:8080/websocket_test.html#
 //https://github.com/LearnBoost/socket.io-spec
 
-app.listen(PORT);
+app.listen(15666);
 
 app.setAppFile = (function () {
    app.get('/', function (req, res) {
